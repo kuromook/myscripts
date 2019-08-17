4c4ae505bb7c32bbbd3bb00e6ee44456c6ba145b
Shunichi Fujiwara <sh1@dream.jp>
2019-08-17 09:45:14 +0900

    clean up

--- blender2core.bat
+++ blender2core.bat
@@ -1,7 +1,2 @@
 start /affinity 3C "" /B "C:\tool\BlenderPortable278\App\Blender64\Blender.exe "
 return
-
-rem start /affinity 3C "" /B "C:\tool\BlenderPortable\App\Blender64\Blender.exe "
-rem return
-rem start /affinity 3F "" /B "C:\tool\BlenderPortable\App\Blender64\Blender.exe "
-rem return
