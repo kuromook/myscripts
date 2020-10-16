94be5c28ebb20985163a83908ce58e34243c3697
kuromook <shunichi.fujwiara.tech@sh1.pw>
2020-10-17 07:34:46 +0900

    zip

--- worksup.py
+++ worksup.py
@@ -1,4 +1,4 @@
-import subprocess
+from  subprocess import call
 import argparse
 import os.path
 
@@ -7,8 +7,8 @@ parser.add_argument('arg1')
 args = parser.parse_args()
 title = args.arg1
 
-subprocess.call(f"magick convert ./{title}/jpg/* ./{title}/{title}.pdf")
+call(f"magick convert ./{title}/jpg/* ./{title}/{title}.pdf")
 if(os.path.exists(f"./{title}/keshi_jpg")):
-    subprocess.call(f"magick convert ./{title}/keshi_jpg/* ./{title}/keshi_{title}.pdf")
-subprocess.call(f"powershell New-Item -ItemType Directory -Force -Path archive")
-subprocess.call(f"powershell compress-archive -Path ./{title} -DestinationPath ./archive/{title}.zip -force")
+    call(f"magick convert ./{title}/keshi_jpg/* ./{title}/keshi_{title}.pdf")
+call(f"powershell New-Item -ItemType Directory -Force -Path archive")
+call(f"powershell compress-archive -Path ./{title} -DestinationPath ./archive/{title}.zip -force")
